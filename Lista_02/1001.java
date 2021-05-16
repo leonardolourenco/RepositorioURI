a6d1f07c3ac5a0b04798f5fb4a83bbda14bb628d
Leonardo <leoz_in@hotmail.com>
2021-05-16 15:36:57 -0300

    1001.java

---
+++ Lista_02/1001.java
@@ -0,0 +1,15 @@
+package aulaherediateste;
+
+import java.util.Scanner;
+
+public class AulaHerediaTeste {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int A = leitor.nextInt();
+        int B = leitor.nextInt();
+        int X = A + B;
+        System.out.println("X = " + X);
+    }
+
+}
