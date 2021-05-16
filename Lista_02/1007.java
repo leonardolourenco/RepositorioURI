9a54730b87c41af9934111cbe1508e9fdf73b954
Leonardo <leoz_in@hotmail.com>
2021-05-16 15:54:59 -0300

    1007

---
+++ Lista_02/1007.java
@@ -0,0 +1,17 @@
+package aulaherediateste;
+
+import java.util.Scanner;
+
+public class AulaHerediaTeste {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int A = leitor.nextInt();
+        int B = leitor.nextInt();
+        int C = leitor.nextInt();
+        int D = leitor.nextInt();
+        int DIFERENCA = (A*B - C*D);
+        System.out.println("DIFERENCA = " + DIFERENCA);
+    }
+
+}
