891c568b2bd7fbe61ab0386e9fb19a3f3b626bcf
Leonardo <leoz_in@hotmail.com>
2021-05-26 18:51:22 -0300

    1078-Tabuada

---
+++ Lista_04/1078.java
@@ -0,0 +1,21 @@
+package aulaheredia;
+
+import java.util.Scanner;
+
+public class AulaHeredia {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        System.out.println("1 x "+ N +" = " + 1*N);
+        System.out.println("2 x "+ N +" = " + 2*N);
+        System.out.println("3 x "+ N +" = " + 3*N);
+        System.out.println("4 x "+ N +" = " + 4*N);
+        System.out.println("5 x "+ N +" = " + 5*N);
+        System.out.println("6 x "+ N +" = " + 6*N);
+        System.out.println("7 x "+ N +" = " + 7*N);
+        System.out.println("8 x "+ N +" = " + 8*N);
+        System.out.println("9 x "+ N +" = " + 9*N);
+        System.out.println("10 x "+ N +" = " + 10*N);
+        }
+   }
